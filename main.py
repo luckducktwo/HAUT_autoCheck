fa419e74dd9c157358855c703a313fac0eba8a18
2917320323 <57084821+2917320323@users.noreply.github.com>
2020-10-08 05:19:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ def GetUserJson(deptId,text,stuNum,userName,RuleId,templateid):
             "deptid": deptId,
             "text": text
         },
-        "areaStr": {"streetNumber":"","street":"长椿路辅路","district":"中原区","city":"郑州市","province":"河南省","town":"","pois":"河南工业大学(莲花街校区)","lng":113.55064699999795 + random.random()/1000,"lat":34.83870696238093 + random.random()/1000,"address":"中原区长椿路辅路河南工业大学(莲花街校区)","text":"河南省-郑州市","code":""},
+        "areaStr": {"streetNumber":"","street":"林山寨街道","district":"中原区","city":"郑州市","province":"河南省","town":"","pois":"河南工业大学(嵩山路校区)","lng":113.2031809999795 + random.random()/1000,"lat":34.74250116238093 + random.random()/1000,"address":"中原区林山寨街道嵩山南路142号河南工业大学(嵩山路校区)","text":"河南省-郑州市","code":""},
         "reportdate": round(time.time()*1000),
         "customerid": "43",
         "deptid": deptId,
